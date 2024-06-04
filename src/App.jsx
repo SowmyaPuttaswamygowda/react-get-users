7f9736b84aa14901ce0fa9fccb37ace97d43cea4
Sowmya Puttaswamygowda <cpsowmya9@gmil.com>
2024-06-04 21:13:48 +0200

    add get user

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header'
+import UserContainer from './components/UserContainer'
+
+function App() {
+
+  return (
+    <div>
+      <Header title="GET-USER" />
+      <UserContainer />
+    </div>
+  )
+}
+
+export default App;
